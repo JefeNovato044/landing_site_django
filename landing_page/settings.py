2e44423a75114669f58b6718b317e8fc41828194
JefeNovato044 <jack@merida.mx>
2021-10-20 14:17:15 -0500

    Update settings.py

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -139,7 +139,7 @@ print("HALO: ",STATIC_ROOT)
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
